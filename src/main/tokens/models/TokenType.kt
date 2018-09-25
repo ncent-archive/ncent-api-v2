e09091a68d07b8914cfdaa4bb8c230de3f6e412c
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-25 15:12:02 -0700

    Add base skeleton for Tokens/TokenTypes

---
+++ src/main/tokens/models/TokenType.kt
@@ -0,0 +1,21 @@
+package main.transactions.models
+
+import kotlinserverless.framework.models.BaseModel
+
+/**
+ * Representation of a TokenType
+ *
+ * @property id
+ * @property name
+ * @property parentToken -- if it is null is it a base nCentToken
+ * @property parentTokenConversionRate this represents how many of this token would be
+ * required to covert to a single parent token. EX: 100 coke tokens -> 1 ncentToken would
+ * mean that the parentTokenConversionRate is 100.
+ */
+data class TokenType(
+        override var id: Int?,
+        var name: String,
+        var parentToken: TokenType?,
+        var parentTokenConversionRate: Double?
+
+) : BaseModel()
--- src/main/transactions/models/Action.kt
+++ src/main/transactions/models/Action.kt
@@ -1,11 +1,13 @@
 package main.transactions.models
 
 /**
- *
+ * Representation of an action taking place and being stored in a transaction
+ * @property type This is the action type; ex: transfer, create, share, payout.
+ * @property data The data object; ex: a particular token, a particular Challenge
+ * @property dataType This is the object type; ex: Token, Challenge.
  */
 data class Action<T>(
         var type: String,
         var data: T?,
-        var dataType: String?,
-        var amount: Int?
+        var dataType: String?
 )
