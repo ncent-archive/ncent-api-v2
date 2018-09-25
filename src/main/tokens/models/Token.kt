bcab802e47e21506d02830331335f36e620bebb6
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-25 15:13:36 -0700

    Add better info for tokens

--- src/main/tokens/models/Token.kt
+++ src/main/tokens/models/Token.kt
@@ -3,7 +3,7 @@ package main.transactions.models
 import kotlinserverless.framework.models.BaseModel
 
 /**
- * Representation of a Token
+ * Representation of a Token -- used when transfering/sharing tokens via transactions
  *
  * @property id
  * @property amount
