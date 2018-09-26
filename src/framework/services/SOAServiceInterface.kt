0e0fcc9b55fe4d017db8241f982dd3608ef695ba
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 15:12:45 -0700

    Add caller to interface to get ID of user calling so validations can happen

--- src/framework/services/SOAServiceInterface.kt
+++ src/framework/services/SOAServiceInterface.kt
@@ -1,34 +1,35 @@
 package kotlinserverless.framework.services
 
 import kotlinserverless.framework.healthchecks.InvalidEndpoint
+import kotlinserverless.framework.models.Model
 
 interface SOAServiceInterface<T> {
-    fun execute() : SOAResult<T> {
+    fun execute(caller: Model) : SOAResult<T> {
         throw InvalidEndpoint()
     }
 
     // GET/UPDATE a single result by id and filters
-    fun execute(id: Int?, params: Map<String, String>?) : SOAResult<T> {
+    fun execute(caller: Model, id: Int?, params: Map<String, String>?) : SOAResult<T> {
         throw InvalidEndpoint()
     }
 
     // GET/UPDATE a single result by key and filters
-    fun execute(key: String?, params: Map<String, String>?) : SOAResult<T> {
+    fun execute(caller: Model, key: String?, params: Map<String, String>?) : SOAResult<T> {
         throw InvalidEndpoint()
     }
 
     // GET/UPDATE multiple results by filters
-    fun execute(params: Map<String, String>?) : SOAResult<List<T>> {
+    fun execute(caller: Model, params: Map<String, String>?) : SOAResult<List<T>> {
         throw InvalidEndpoint()
     }
 
     // CREATE/UPDATE a single object
-    fun execute(data: T?, params: Map<String, String>?) : SOAResult<T> {
+    fun execute(caller: Model, data: T?, params: Map<String, String>?) : SOAResult<T> {
         throw InvalidEndpoint()
     }
 
     // CREATE/UPDATE multiple objects
-    fun execute(dataList: List<T>?, params: Map<String, String>?) : SOAResult<List<T>> {
+    fun execute(caller: Model, dataList: List<T>?, params: Map<String, String>?) : SOAResult<List<T>> {
         throw InvalidEndpoint()
     }
 }
--- src/main/users/services/UserHelloService.kt
+++ src/main/users/services/UserHelloService.kt
@@ -3,9 +3,10 @@ package kotlinserverless.main.users.services
 import kotlinserverless.framework.services.SOAResult
 import kotlinserverless.framework.services.SOAResultType
 import kotlinserverless.framework.services.SOAServiceInterface
+import kotlinserverless.framework.models.Model
 
 class UserHelloService: SOAServiceInterface<String> {
-	override fun execute(data: String?, params: Map<String, String>?) : SOAResult<String> {
+	override fun execute(caller: Model, data: String?, params: Map<String, String>?) : SOAResult<String> {
 		return SOAResult(SOAResultType.SUCCESS, null, "HELLO WORLD")
 	}
 }
