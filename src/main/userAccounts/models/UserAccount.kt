24d41b2e26e03c154611fa98f7ec2fd62df8a9b9
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 15:05:18 -0700

    Continue updating services

--- src/main/userAccounts/models/UserAccount.kt
+++ src/main/userAccounts/models/UserAccount.kt
@@ -8,23 +8,47 @@ import kotlinserverless.framework.models.BaseModel
  * their wallet information, and the api information. Services will include:
  * generation, validation, and throttling.
  * @property id
+ * @property userMetadata UserMetadata
+ * @property cryptoKeyPair CryptoKeyPair
+ * @property apiCreds ApiCreds
+ */
+data class UserAccount(
+        override var id: Int?,
+        var userMetadata: UserMetadata,
+        var cryptoKeyPair: CryptoKeyPair,
+        var apiCreds: ApiCreds
+) : BaseModel()
+
+/**
+ * User information
  * @property email
  * @property firstName
  * @property lastName
  * @property userName
- * @property publicKey
- * @property encryptedPrivateKey
- * @property apiKey
- * @property encryptedSecretKey
  */
-data class UserAccount(
-        override var id: Int?,
+data class UserMetadata(
         var email: String,
         var firstName: String,
         var lastName: String,
-        var userName: String,
+        var userName: String
+)
+
+/**
+ * Api credentials
+ * @property publicKey
+ * @property encryptedPrivateKey
+ */
+data class CryptoKeyPair(
         var publicKey: String,
-        var encryptedPrivateKey: String,
+        var encryptedPrivateKey: String
+)
+
+/**
+ * Api credentials
+ * @property apiKey
+ * @property encryptedSecretKey
+ */
+data class ApiCreds(
         var apiKey: String,
         var encryptedSecretKey: String
-) : BaseModel()
+)
