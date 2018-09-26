7940b410f45a59dd5ce2404de3ea67973e8793b1
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 15:51:49 -0700

    dding token services

--- src/main/tokens/services/GetTokensService.kt
+++ src/main/tokens/services/GetTokensService.kt
@@ -1,2 +1,14 @@
 package main.tokens.services
 
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAServiceInterface
+import main.tokens.models.Token
+
+/**
+ * Retrieve tokens based on a filter (ex: id, type)
+ */
+class GetTokensService: SOAServiceInterface<Token> {
+    override fun execute(caller: Int?, params: Map<String, String>?) : SOAResult<List<Token>> {
+        throw NotImplementedError()
+    }
+}a
