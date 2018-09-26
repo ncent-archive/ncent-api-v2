89d47085d9b391cd8c409b4230658d448cbcf347
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 16:09:35 -0700

    Setup reward service

--- src/main/rewards/services/GenerateRewardService.kt
+++ src/main/rewards/services/GenerateRewardService.kt
@@ -1,2 +1,14 @@
 package main.rewards.services
 
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAServiceInterface
+import main.rewards.models.Reward
+
+/**
+ * Generate a reward if it is valid
+ */
+class GenerateRewardService: SOAServiceInterface<Reward> {
+    override fun execute(caller: Int?, data: Reward?, params: Map<String, String>?) : SOAResult<Reward> {
+        throw NotImplementedError()
+    }
+}
