be92759a50816343740ae9a4c5b46ac0f69c6bf0
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 11:06:47 -0700

    CLeanup challenges and reward and add more services

--- src/main/completionCriterias/models/CompletionCriteria.kt
+++ src/main/completionCriterias/models/CompletionCriteria.kt
@@ -2,7 +2,15 @@ package main.completionCriterias.models
 
 import kotlinserverless.framework.models.BaseModel
 
+/**
+ * Currently only housing who can validate the completion of a challenge
+ * Eventually this will house things like smart contract completers.
+ *
+ * @property id
+ * @property address The address which can trigger validation of completion
+ */
 data class CompletionCriteria(
-        override var id: Int?
+        override var id: Int?,
+        val address: String
 
 ) : BaseModel()
