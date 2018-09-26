a3134018666fbbe800e0a413b5d17afe38cef0e6
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 14:52:12 -0700

    Fix example service for new interface

--- src/main/users/services/UserHelloService.kt
+++ src/main/users/services/UserHelloService.kt
@@ -3,10 +3,9 @@ package kotlinserverless.main.users.services
 import kotlinserverless.framework.services.SOAResult
 import kotlinserverless.framework.services.SOAResultType
 import kotlinserverless.framework.services.SOAServiceInterface
-import kotlinserverless.main.users.models.User
 
-class UserHelloService: SOAServiceInterface<User> {
-	fun execute(data: String?, params: Map<String, String>?) : SOAResult<String> {
+class UserHelloService: SOAServiceInterface<String> {
+	override fun execute(data: String?, params: Map<String, String>?) : SOAResult<String> {
 		return SOAResult(SOAResultType.SUCCESS, null, "HELLO WORLD")
 	}
 }
