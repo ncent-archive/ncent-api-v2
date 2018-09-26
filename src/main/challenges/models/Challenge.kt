b080d7a6fefe2dabdb72c9f59b636fcfaab770d0
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 11:23:50 -0700

    Add additional documentation

--- src/main/challenges/models/Challenge.kt
+++ src/main/challenges/models/Challenge.kt
@@ -10,19 +10,9 @@ import org.joda.time.DateTime
  * store the state for a challenge over the lifecycle of that challenge
  *
  * @property id
- * @property name
- * @property expiration
- * @property admin address of owner of this challenge
- * @property offChain optionally set this challenge to allow off-chain sharing
- * @property maxRewards the number of times the reward can be claimed
- * @property maxDistributionFeeReward the number of times the distribution fee
- * can be rewarded
- * @property synchronousChallenges sub challenges that must be completed in order
- * @property asynchronousChallenges sub challenges that can be completed in any order
- * @property completionCriteria used to validate completion, ex: oracle, admin, contract
- * @property reward the reward amount and it's pool
- * @property distributionFeeReward the distribution fees and the pool. this is the
- * pool that will be drawn on if anybody 'opts-out' of attempting to help.
+ * @property challengeSettings ChallengeSettings
+ * @property subChallenges SubChallenges
+ * @property resultVectors ResultVectors
  */
 data class Challenge(
         override var id: Int?,
