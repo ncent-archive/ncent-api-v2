f074d6c143bdc25968740352dbb0af659b12dd94
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 16:22:42 -0700

    completion criteria services

--- src/main/completionCriterias/services/ValidateCompletionCriteriaService.kt
+++ src/main/completionCriterias/services/ValidateCompletionCriteriaService.kt
@@ -1,2 +1,14 @@
 package main.completionCriterias.services
 
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAServiceInterface
+import main.completionCriterias.models.CompletionCriteria
+
+/**
+ * For the time being, just validate the caller is the criteria address
+ */
+class ValidateCompletionCriteriaService: SOAServiceInterface<CompletionCriteria> {
+    override fun execute(caller: Int?, data: CompletionCriteria?, params: Map<String, String>?) : SOAResult<CompletionCriteria> {
+        throw NotImplementedError()
+    }
+}
