36ab6462dd88412975f63801b5a8ad58e2ce352d
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 13:58:44 -0700

    Add additional execution types

--- src/framework/services/SOAServiceInterface.kt
+++ src/framework/services/SOAServiceInterface.kt
@@ -3,11 +3,28 @@ package kotlinserverless.framework.services
 import kotlinserverless.framework.healthchecks.InvalidEndpoint
 
 interface SOAServiceInterface<T> {
+    // GET/UPDATE a single result by id and filters
+    fun execute(id: Int?, params: Map<String, String>?) : SOAResult<T> {
+        throw InvalidEndpoint()
+    }
+
+    // GET/UPDATE a single result by key and filters
+    fun execute(key: String?, params: Map<String, String>?) : SOAResult<T> {
+        throw InvalidEndpoint()
+    }
+
+    // GET/UPDATE multiple results by filters
+    fun execute(params: Map<String, String>?) : SOAResult<List<T>> {
+        throw InvalidEndpoint()
+    }
+
+    // CREATE/UPDATE a single object
     fun execute(data: T?, params: Map<String, String>?) : SOAResult<T> {
         throw InvalidEndpoint()
     }
 
-    fun execute(dataList: List<T>?, params: Map<String, String>?) : SOAResult<T> {
+    // CREATE/UPDATE multiple objects
+    fun execute(dataList: List<T>?, params: Map<String, String>?) : SOAResult<List<T>> {
         throw InvalidEndpoint()
     }
 }
