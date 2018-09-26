4837e87b176f4c6e65c1c3400bc0eceae2a820f1
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 15:14:18 -0700

    Working on generate api creds service

--- src/main/userAccounts/services/GenerateApiCredsService.kt
+++ src/main/userAccounts/services/GenerateApiCredsService.kt
@@ -4,8 +4,11 @@ import kotlinserverless.framework.services.SOAResult
 import kotlinserverless.framework.services.SOAServiceInterface
 import main.userAccounts.models.ApiCreds
 
+/**
+ * This service will be used to generate Api credentials
+ */
 class GenerateApiCredsService: SOAServiceInterface<ApiCreds> {
-    override fun execute(data: ApiCreds?, params: Map<String, String>?) : SOAResult<ApiCreds> {
+    override fun execute(caller: Int?) : SOAResult<ApiCreds> {
         throw NotImplementedError()
     }
 }
