90ec5ad5528b2d4c84f7081b4f3b8c0d3b7215bc
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 15:55:44 -0700

    adding token services

--- src/main/tokens/services/GetTokensService.kt
+++ src/main/tokens/services/GetTokensService.kt
@@ -3,12 +3,13 @@ package main.tokens.services
 import kotlinserverless.framework.services.SOAResult
 import kotlinserverless.framework.services.SOAServiceInterface
 import main.tokens.models.Token
+import kotlinserverless.framework.models.BaseModel
 
 /**
- * Retrieve tokens based on a filter (ex: id, type)
+ * Retrieve tokens based on a filtered type (value being the TokenType)
  */
 class GetTokensService: SOAServiceInterface<Token> {
-    override fun execute(caller: Int?, params: Map<String, String>?) : SOAResult<List<Token>> {
+    override fun execute(caller: Int?, key: String?, value: BaseModel?) : SOAResult<List<Token>> {
         throw NotImplementedError()
     }
-}a
+}
--- src/main/tokens/services/TransferTokenService.kt
+++ src/main/tokens/services/TransferTokenService.kt
@@ -1,2 +1,15 @@
 package main.tokens.services
 
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAServiceInterface
+import main.tokens.models.Token
+import main.transactions.models.Transaction
+
+/**
+ * Transfer tokens from one address to another
+ */
+class TransferTokenService: SOAServiceInterface<Transaction<Token>> {
+    override fun execute(caller: Int?, data: Transaction<Token>?, params: Map<String, String>?) : SOAResult<Transaction<Token>> {
+        throw NotImplementedError()
+    }
+}
