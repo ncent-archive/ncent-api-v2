60b31ace63b7495a04e1ab55e3123e88bc977644
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 11:23:50 -0700

    Add additional documentation

--- src/main/rewards/models/RewardType.kt
+++ src/main/rewards/models/RewardType.kt
@@ -2,7 +2,15 @@ package main.rewards.models
 
 import kotlinserverless.framework.models.BaseModel
 
+/**
+ * RewardType signifies the way the rewards will be distributed amongst the entire chain or the providence chain
+ *
+ * @property id
+ * @property audience example: [PROVIDENCE, FULL]
+ * @property type example: [SINGLE, EVEN, TIERED, LOGARITHMIC, EXPONENTIAL_UP, EXPONENTIAL_DOWN]
+ */
 data class RewardType(
         override var id: Int?,
+        val audience: String,
         val type: String
 ) : BaseModel()
