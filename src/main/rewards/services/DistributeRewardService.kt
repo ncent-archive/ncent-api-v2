4bc1c39c9253ecf5d2e835f19a12f2178c1e8c6f
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-09-26 16:09:35 -0700

    Setup reward service

--- src/main/rewards/services/DistributeRewardService.kt
+++ src/main/rewards/services/DistributeRewardService.kt
@@ -1,2 +1,15 @@
 package main.rewards.services
 
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAServiceInterface
+import main.rewards.models.Reward
+import main.transactions.models.Transaction
+
+/**
+ * Transfer tokens based on rewards
+ */
+class DistributeRewardService: SOAServiceInterface<Transaction<Reward>> {
+    override fun execute(caller: Int?, data: Transaction<Reward>?, params: Map<String, String>?) : SOAResult<Transaction<Reward>> {
+        throw NotImplementedError()
+    }
+}
