18704d393bf5f0b95b28ea15a6d63ab5a89cc3bc
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-10-11 16:39:54 -0700

    Update controllers and update challenge model...still need to update the rest of the models for the new DAO interface

--- src/main/tokens/controllers/TokenController.kt
+++ src/main/tokens/controllers/TokenController.kt
@@ -3,21 +3,21 @@ package main.tokens.controllers
 
 import kotlinserverless.framework.controllers.RestController
 import kotlinserverless.framework.controllers.DefaultController
-import kotlinserverless.framework.models.ApiUser
 import kotlinserverless.framework.services.SOAResult
 import main.tokens.models.Token
 import kotlinserverless.framework.models.Request
+import kotlinserverless.main.users.models.User
 
-class TokenController: DefaultController<Token>(), RestController<Token, ApiUser> {
-    override fun findOne(user: ApiUser, id: Int): SOAResult<Token> {
+class TokenController: DefaultController<Token>(), RestController<Token, User> {
+    override fun findOne(user: User, id: Int): SOAResult<Token> {
         throw NotImplementedError()
     }
 
-    override fun create(user: ApiUser, element: Token): SOAResult<Token> {
+    override fun create(user: User, element: Token): SOAResult<Token> {
         throw NotImplementedError()
     }
 
-    fun transfer(user: ApiUser, request: Request): SOAResult<Token> {
+    fun transfer(user: User, request: Request): SOAResult<Token> {
         throw NotImplementedError()
     }
 }
