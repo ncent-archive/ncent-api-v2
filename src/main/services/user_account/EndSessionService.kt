fffdd0b8930b8932f6d1f032d312bb75007acf4d
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-10-31 14:22:59 -0700

    End session service and tests

--- src/main/services/user_account/EndSessionService.kt
+++ src/main/services/user_account/EndSessionService.kt
@@ -1,14 +1,23 @@
 package main.services.user_account
 
+import framework.services.DaoService
 import kotlinserverless.framework.services.SOAResult
 import kotlinserverless.framework.services.SOAServiceInterface
 import main.daos.Session
+import main.daos.Sessions
+import org.joda.time.DateTime
 
 /**
  * Used to end a session (cache)
  */
 class EndSessionService: SOAServiceInterface<Session> {
     override fun execute(caller: Int?, key: String?) : SOAResult<Session> {
-        throw NotImplementedError()
+        return DaoService<Session>().execute {
+            var session = Session.find { Sessions.sessionKey eq key!! }.first()
+            if(session.expiration > DateTime.now()) {
+                session.expiration = DateTime.now()
+            }
+            return@execute session
+        }
     }
 }
