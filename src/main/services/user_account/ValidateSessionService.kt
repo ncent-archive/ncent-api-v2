9c4f3782fee98ea5a0d6edfdce86e9e3602b39f5
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-10-31 15:22:17 -0700

    Add validate session service and tests

--- src/main/services/user_account/ValidateSessionService.kt
+++ src/main/services/user_account/ValidateSessionService.kt
@@ -1,14 +1,38 @@
 package main.services.user_account
 
+import framework.models.idValue
 import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAResultType
 import kotlinserverless.framework.services.SOAServiceInterface
 import main.daos.Session
+import main.daos.UserAccount
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 
 /**
  * Validate the session
  */
 class ValidateSessionService: SOAServiceInterface<Session> {
     override fun execute(caller: Int?, key: String?) : SOAResult<Session> {
-        throw NotImplementedError()
+        var result = SOAResult<Session>(
+            SOAResultType.FAILURE,
+            "",
+            null
+        )
+        return transaction {
+            val user = UserAccount.findById(caller!!)!!
+            val session = Session.findById(user.session)
+            if(session == null) {
+                result.message = "Session not found"
+            } else if(session!!.sessionKey != key!!) {
+                result.message = "Invalid Session"
+            } else if(session!!.expiration <= DateTime.now()) {
+                result.message = "Session Expired"
+            } else {
+                result.data = session
+                result.result = SOAResultType.SUCCESS
+            }
+            return@transaction result
+        }
     }
 }
---
+++ src/test/unit/services/user_account/ValidateSessionServiceTest.kt
@@ -0,0 +1,67 @@
+package test.unit.services.user_account
+
+import framework.models.idValue
+import io.kotlintest.*
+import io.kotlintest.specs.WordSpec
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import main.daos.*
+import kotlinserverless.framework.models.Handler
+import kotlinserverless.framework.services.SOAResultType
+import main.services.user_account.EndSessionService
+import main.services.user_account.GenerateUserAccountService
+import main.services.user_account.ValidateSessionService
+import org.jetbrains.exposed.sql.transactions.transaction
+
+@ExtendWith(MockKExtension::class)
+class ValidateSessionServiceTest : WordSpec() {
+    private var service = ValidateSessionService()
+    private var params = mutableMapOf(
+            Pair("email", "dev@ncnt.io"),
+            Pair("firstname", "dev"),
+            Pair("lastname", "ncnt")
+    )
+    private lateinit var session: Session
+    private lateinit var user: UserAccount
+
+    override fun beforeTest(description: Description): Unit {
+        Handler.connectAndBuildTables()
+    }
+
+    override fun afterTest(description: Description, result: TestResult) {
+        Handler.disconnectAndDropTables()
+    }
+
+    init {
+        "executing validate session service" should {
+            "should return valid for a valid session" {
+                transaction {
+                    user = GenerateUserAccountService().execute(null, params).data!!
+                    session = Session.findById(user.session)!!
+                }
+
+                var result = service.execute(user.idValue, session.sessionKey)
+                result.result shouldBe SOAResultType.SUCCESS
+            }
+            "should return invalid for a sessionkey that is not associated with the caller" {
+                transaction {
+                    user = GenerateUserAccountService().execute(null, params).data!!
+                    session = Session.findById(user.session)!!
+                }
+                var result = service.execute(user.idValue, "SOMERANDOMKEY")
+                result.result shouldBe SOAResultType.FAILURE
+                result.message shouldBe "Invalid Session"
+            }
+            "should return invalid for an expired sessionkey" {
+                transaction {
+                    user = GenerateUserAccountService().execute(null, params).data!!
+                    session = Session.findById(user.session)!!
+                }
+                EndSessionService().execute(null, session.sessionKey)
+                var result = service.execute(user.idValue, session.sessionKey)
+                result.result shouldBe SOAResultType.FAILURE
+                result.message shouldBe "Session Expired"
+            }
+        }
+    }
+}
