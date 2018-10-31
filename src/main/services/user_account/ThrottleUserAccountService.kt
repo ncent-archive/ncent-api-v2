ff817b1dd34e9bdb597acb32084482df28518f49
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-10-31 15:22:17 -0700

    Add validate session service and tests

--- src/main/services/user_account/ThrottleUserAccountService.kt
+++ src/main/services/user_account/ThrottleUserAccountService.kt
@@ -6,6 +6,10 @@ import main.daos.UserAccount
 
 /**
  * Check if we need to throttle the user
+ * We should use a cache to keep track of requests attempted each second/minute/hour
+ * We should throttle users based on a hard cap
+ * We should allow whitelists for specific users
+ * We should allow editable limits for some as well
  */
 class ThrottleUserAccountService: SOAServiceInterface<UserAccount> {
     override fun execute(caller: Int?, data: Any?, params: Map<String, String>?) : SOAResult<UserAccount> {
