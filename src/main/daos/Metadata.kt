a86580fffc8f32415bd0a0347bbdc5d3735b8f46
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-11-05 16:21:10 -0800

    Add metadata to user and also show using table references

---
+++ src/main/daos/Metadata.kt
@@ -0,0 +1,29 @@
+package main.daos
+
+import framework.models.BaseIntEntity
+import framework.models.BaseIntEntityClass
+import framework.models.BaseIntIdTable
+import org.jetbrains.exposed.dao.EntityID
+
+/**
+ * Metadata will be a key-value store
+ *
+ * @property id
+ * @property key
+ * @property value
+ */
+class Metadata(id: EntityID<Int>) : BaseIntEntity(id, Metadatas) {
+    companion object : BaseIntEntityClass<Metadata>(Metadatas)
+
+    var key by Metadatas.key
+    var value by Metadatas.value
+}
+
+object Metadatas : BaseIntIdTable("metadatas") {
+    val key = varchar("key", 256)
+    val value = varchar("value", 256)
+}
+
+data class MetadatasNamespace(val key: String, val value: String)
+
+data class MetadatasListNamespace(val metadatas: List<MetadatasNamespace>)
--- src/main/daos/User.kt
+++ src/main/daos/User.kt
@@ -4,6 +4,7 @@ import framework.models.BaseIntEntity
 import framework.models.BaseIntEntityClass
 import framework.models.BaseIntIdTable
 import org.jetbrains.exposed.dao.*
+import org.jetbrains.exposed.sql.Table
 
 /**
  * Basic fields that a User needs
@@ -11,6 +12,7 @@ import org.jetbrains.exposed.dao.*
  * @property email User email
  * @property firstname User first name
  * @property lastname User last name
+ * @property metadata Metadata used to track additional information about the user
  */
 class User(id: EntityID<Int>) : BaseIntEntity(id, Users) {
 	companion object : BaseIntEntityClass<User>(Users)
@@ -18,10 +20,16 @@ class User(id: EntityID<Int>) : BaseIntEntity(id, Users) {
 	var email by Users.email
 	var firstname by Users.firstname
 	var lastname by Users.lastname
+	var metadatas by Metadata via UsersMetadata
 }
 
 object Users : BaseIntIdTable("users") {
 	val email = varchar("email", 50).uniqueIndex()
 	val firstname = varchar("firstname", 20)
 	val lastname = varchar("lastname", 20)
+}
+
+object UsersMetadata : Table("users_to_metadatas") {
+	val user = reference("user_to_metadatas", Users).primaryKey(0)
+	val metadata = reference("metadata_to_transaction", Metadatas).primaryKey(1)
 }
