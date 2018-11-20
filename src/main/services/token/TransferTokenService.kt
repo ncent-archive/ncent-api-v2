7f14eebc86c5b97b232a99e5503e775bee559375
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-11-19 16:46:05 -0800

    Add comments for todo for transfer token service

--- src/main/services/token/TransferTokenService.kt
+++ src/main/services/token/TransferTokenService.kt
@@ -9,6 +9,12 @@ import main.daos.Transaction
  */
 class TransferTokenService: SOAServiceInterface<Transaction> {
     override fun execute(caller: Int?, data: Any?, params: Map<String, String>?) : SOAResult<Transaction> {
+        // TODO: verify that the caller is the from address
+        // TODO: verify they have enough funds and that the token itself is still valid
+        // TODO: -- this can be done by getting all of the transactions for a user for a particular token
+        // TODO: -- then calculating their balance
+        // TODO: verify that the to address exists
+        // TODO: Add a transaction transfering funds
         throw NotImplementedError()
     }
 }
