2e5aeda76d4d027bbe4654b539db3f2bedc6c8c1
Arya Soltanieh <soltanieharya+github@gmail.com>
2018-12-12 15:00:04 -0800

    remove the state machine lib, add a bootleg state machine, add service to change challenge state, tests still needed

--- src/main/daos/Challenge.kt
+++ src/main/daos/Challenge.kt
@@ -1,16 +1,10 @@
 package main.daos
 
-import com.tinder.StateMachine
 import framework.models.BaseIntEntity
 import framework.models.BaseIntEntityClass
 import framework.models.BaseIntIdTable
 import framework.models.idValue
-import framework.services.DaoService
-import kotlinserverless.framework.models.Handler
-import kotlinserverless.framework.services.SOAResultType
-import main.services.transaction.GenerateTransactionService
 import main.services.transaction.GetTransactionsService
-import org.apache.log4j.Priority
 import org.jetbrains.exposed.dao.*
 
 /**
@@ -35,54 +29,87 @@ class Challenge(id: EntityID<Int>) : BaseIntEntity(id, Challenges) {
     var cryptoKeyPair by CryptoKeyPair referencedOn Challenges.cryptoKeyPair
     var distributionFeeReward by Reward referencedOn Challenges.distributionFeeReward
 
-    val stateMachine = StateMachine.create<ChallengeState, ChallengeEvent, ChallengeSideEffect> {
-        initialState(ChallengeState.CREATED)
-        state<ChallengeState.CREATED> {
-            onEnter {
-                ChallengeSideEffect.LogCreated
+    fun canTransitionState(fromState: ActionType, toState: ActionType): Boolean {
+        return when(fromState) {
+            ActionType.COMPLETE -> {
+                false
             }
-            on<ChallengeEvent.OnActivated> {
-                transitionTo(ChallengeState.ACTIVE, ChallengeSideEffect.LogActivated)
+            ActionType.CREATE -> {
+                return arrayListOf(
+                    ActionType.ACTIVATE,
+                    ActionType.EXPIRE,
+                    ActionType.INVALIDATE
+                ).contains(toState)
             }
-            on<ChallengeEvent.OnInvalidated> {
-                transitionTo(ChallengeState.INVALID, ChallengeSideEffect.LogInvalidated)
+            ActionType.EXPIRE -> {
+                false
             }
-            on<ChallengeEvent.OnExpired> {
-                transitionTo(ChallengeState.EXPIRED, ChallengeSideEffect.LogExpired)
+            ActionType.INVALIDATE -> {
+                return arrayListOf(
+                    ActionType.ACTIVATE,
+                    ActionType.EXPIRE
+                ).contains(toState)
             }
-
-        }
-        state<ChallengeState.ACTIVE> {
-            on<ChallengeEvent.OnCompleted> {
-                transitionTo(ChallengeState.COMPLETE, ChallengeSideEffect.LogCompleted)
+            ActionType.ACTIVATE -> {
+                return arrayListOf(
+                    ActionType.COMPLETE,
+                    ActionType.EXPIRE,
+                    ActionType.INVALIDATE
+                ).contains(toState)
             }
-            on<ChallengeEvent.OnInvalidated> {
-                transitionTo(ChallengeState.INVALID, ChallengeSideEffect.LogInvalidated)
-            }
-            on<ChallengeEvent.OnExpired> {
-                transitionTo(ChallengeState.EXPIRED, ChallengeSideEffect.LogExpired)
-            }
-
-        }
-        state<ChallengeState.INVALID> {
-            on<ChallengeEvent.OnActivated> {
-                transitionTo(ChallengeState.ACTIVE, ChallengeSideEffect.LogActivated)
-            }
-            on<ChallengeEvent.OnExpired> {
-                transitionTo(ChallengeState.EXPIRED, ChallengeSideEffect.LogExpired)
-            }
-        }
-        onTransition {
-            val validTransition = it as? StateMachine.Transition.Valid ?: return@onTransition
-            when (validTransition.sideEffect) {
-                ChallengeSideEffect.LogCreated -> logAndGenerateTransaction(it, ActionType.CREATE)
-                ChallengeSideEffect.LogActivated -> logAndGenerateTransaction(it, ActionType.ACTIVATE)
-                ChallengeSideEffect.LogCompleted -> logAndGenerateTransaction(it, ActionType.COMPLETE)
-                ChallengeSideEffect.LogInvalidated -> logAndGenerateTransaction(it, ActionType.INVALIDATE)
-                ChallengeSideEffect.LogExpired -> logAndGenerateTransaction(it, ActionType.EXPIRE)
-            }
+            else -> throw Exception("Could not find challenge state to move to for ${fromState.type}")
         }
     }
+
+// TODO Look into how we can use the state machine
+//    val stateMachine = StateMachine.create<ChallengeState, ChallengeEvent, ChallengeSideEffect> {
+//        initialState(ChallengeState.CREATED)
+//        state<ChallengeState.CREATED> {
+//            onEnter {
+//                ChallengeSideEffect.LogCreated
+//            }
+//            on<ChallengeEvent.OnActivated> {
+//                transitionTo(ChallengeState.ACTIVE, ChallengeSideEffect.LogActivated)
+//            }
+//            on<ChallengeEvent.OnInvalidated> {
+//                transitionTo(ChallengeState.INVALID, ChallengeSideEffect.LogInvalidated)
+//            }
+//            on<ChallengeEvent.OnExpired> {
+//                transitionTo(ChallengeState.EXPIRED, ChallengeSideEffect.LogExpired)
+//            }
+//
+//        }
+//        state<ChallengeState.ACTIVE> {
+//            on<ChallengeEvent.OnCompleted> {
+//                transitionTo(ChallengeState.COMPLETE, ChallengeSideEffect.LogCompleted)
+//            }
+//            on<ChallengeEvent.OnInvalidated> {
+//                transitionTo(ChallengeState.INVALID, ChallengeSideEffect.LogInvalidated)
+//            }
+//            on<ChallengeEvent.OnExpired> {
+//                transitionTo(ChallengeState.EXPIRED, ChallengeSideEffect.LogExpired)
+//            }
+//
+//        }
+//        state<ChallengeState.INVALID> {
+//            on<ChallengeEvent.OnActivated> {
+//                transitionTo(ChallengeState.ACTIVE, ChallengeSideEffect.LogActivated)
+//            }
+//            on<ChallengeEvent.OnExpired> {
+//                transitionTo(ChallengeState.EXPIRED, ChallengeSideEffect.LogExpired)
+//            }
+//        }
+//        onTransition {
+//            val validTransition = it as? StateMachine.Transition.Valid ?: return@onTransition
+//            when (validTransition.sideEffect) {
+//                ChallengeSideEffect.LogCreated -> logAndGenerateTransaction(it, ActionType.CREATE)
+//                ChallengeSideEffect.LogActivated -> logAndGenerateTransaction(it, ActionType.ACTIVATE)
+//                ChallengeSideEffect.LogCompleted -> logAndGenerateTransaction(it, ActionType.COMPLETE)
+//                ChallengeSideEffect.LogInvalidated -> logAndGenerateTransaction(it, ActionType.INVALIDATE)
+//                ChallengeSideEffect.LogExpired -> logAndGenerateTransaction(it, ActionType.EXPIRE)
+//            }
+//        }
+//    }
 
     val stateChangeActionTypes = setOf(ActionType.CREATE, ActionType.ACTIVATE, ActionType.COMPLETE, ActionType.INVALIDATE, ActionType.EXPIRE)
 
@@ -104,39 +131,40 @@ class Challenge(id: EntityID<Int>) : BaseIntEntity(id, Challenges) {
         return null
     }
 
-    private fun logAndGenerateTransaction(stateTransition: StateMachine.Transition<ChallengeState, ChallengeEvent, ChallengeSideEffect>, state: ActionType): Transaction {
-        val previousTransactionId = getLastStateChangeTransaction()?.idValue
-        val transactionResult = DaoService.execute {
-            val transactionNamespace = TransactionNamespace(
-                from = cryptoKeyPair.publicKey,
-                to = null,
-                action = ActionNamespace(
-                    type = state,
-                    data = idValue,
-                    dataType = "Challenge"
-                ),
-                previousTransaction = previousTransactionId,
-                metadatas = null
-            )
-            val transactionResult = GenerateTransactionService.execute(null, transactionNamespace, null)
-            if(transactionResult != SOAResultType.SUCCESS)
-                throw Exception(transactionResult.message)
-            Handler.log().log(
-                    Priority.INFO,
-                    "Transitioning state for challenge {" +
-                                "id: ${idValue}, " +
-                                "fromState: ${stateTransition.fromState}, " +
-                                "toState: ${state}, " +
-                                "previousTransactionId: ${previousTransactionId}, " +
-                                "transactionId: ${transactionResult.data!!.idValue} " +
-                            "}"
-            )
-            return@execute transactionResult.data!!
-        }
-        if(transactionResult != SOAResultType.SUCCESS)
-            throw Exception(transactionResult.message)
-        return transactionResult.data!!
-    }
+// TODO if we want to use state machine use this:
+//    private fun logAndGenerateTransaction(stateTransition: StateMachine.Transition<ChallengeState, ChallengeEvent, ChallengeSideEffect>, state: ActionType): Transaction {
+//        val previousTransactionId = getLastStateChangeTransaction()?.idValue
+//        val transactionResult = DaoService.execute {
+//            val transactionNamespace = TransactionNamespace(
+//                from = cryptoKeyPair.publicKey,
+//                to = null,
+//                action = ActionNamespace(
+//                    type = state,
+//                    data = idValue,
+//                    dataType = "Challenge"
+//                ),
+//                previousTransaction = previousTransactionId,
+//                metadatas = null
+//            )
+//            val transactionResult = GenerateTransactionService.execute(null, transactionNamespace, null)
+//            if(transactionResult != SOAResultType.SUCCESS)
+//                throw Exception(transactionResult.message)
+//            Handler.log().log(
+//                    Priority.INFO,
+//                    "Transitioning state for challenge {" +
+//                                "id: ${idValue}, " +
+//                                "fromState: ${stateTransition.fromState}, " +
+//                                "toState: ${state}, " +
+//                                "previousTransactionId: ${previousTransactionId}, " +
+//                                "transactionId: ${transactionResult.data!!.idValue} " +
+//                            "}"
+//            )
+//            return@execute transactionResult.data!!
+//        }
+//        if(transactionResult != SOAResultType.SUCCESS)
+//            throw Exception(transactionResult.message)
+//        return transactionResult.data!!
+//    }
 }
 
 class ChallengeList(val challenges: List<Challenge>)
@@ -206,26 +234,27 @@ data class ChallengeMetadata(
  * INVALID -- if there is something wrong with this challenge (ex: funds depleted)
  * EXPIRED -- [final]
  */
-sealed class ChallengeState {
-    object CREATED : ChallengeState()
-    object ACTIVE : ChallengeState()
-    object COMPLETE : ChallengeState()
-    object INVALID : ChallengeState()
-    object EXPIRED : ChallengeState()
-}
-
-sealed class ChallengeEvent {
-    object OnCreated : ChallengeEvent()
-    object OnActivated : ChallengeEvent()
-    object OnCompleted : ChallengeEvent()
-    object OnInvalidated : ChallengeEvent()
-    object OnExpired : ChallengeEvent()
-}
-
-sealed class ChallengeSideEffect {
-    object LogCreated : ChallengeSideEffect()
-    object LogActivated : ChallengeSideEffect()
-    object LogCompleted : ChallengeSideEffect()
-    object LogInvalidated : ChallengeSideEffect()
-    object LogExpired : ChallengeSideEffect()
-}
+// TODO figure out if we want to use a state machine
+//sealed class ChallengeState {
+//    object CREATED : ChallengeState()
+//    object ACTIVE : ChallengeState()
+//    object COMPLETE : ChallengeState()
+//    object INVALID : ChallengeState()
+//    object EXPIRED : ChallengeState()
+//}
+//
+//sealed class ChallengeEvent {
+//    object OnCreated : ChallengeEvent()
+//    object OnActivated : ChallengeEvent()
+//    object OnCompleted : ChallengeEvent()
+//    object OnInvalidated : ChallengeEvent()
+//    object OnExpired : ChallengeEvent()
+//}
+//
+//sealed class ChallengeSideEffect {
+//    object LogCreated : ChallengeSideEffect()
+//    object LogActivated : ChallengeSideEffect()
+//    object LogCompleted : ChallengeSideEffect()
+//    object LogInvalidated : ChallengeSideEffect()
+//    object LogExpired : ChallengeSideEffect()
+//}
--- src/main/services/challenge/ChangeChallengeStateService.kt
+++ src/main/services/challenge/ChangeChallengeStateService.kt
@@ -1,14 +1,45 @@
 package main.services.challenge
 
+import framework.models.idValue
+import framework.services.DaoService
 import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAResultType
 import kotlinserverless.framework.services.SOAServiceInterface
-import main.daos.Transaction
+import main.daos.*
+import main.services.transaction.GenerateTransactionService
 
 /**
  * Trigger a challenge state change.
  */
 object ChangeChallengeStateService: SOAServiceInterface<Transaction> {
     override fun execute(caller: Int?, params: Map<String, String>?) : SOAResult<Transaction> {
-        throw NotImplementedError()
+        return DaoService.execute {
+            val userAccount = UserAccount.findById(caller!!)!!
+            val challenge = Challenge.findById(params!!["challengeId"]!!.toInt())!!
+            if(challenge.challengeSettings.admin != userAccount.id)
+                throw Exception("This user cannot change the challenge state")
+            val newState = ActionType.valueOf(params!!["state"]!!)
+            val oldTx = challenge.getLastStateChangeTransaction()!!
+            val oldState = oldTx.action.type
+
+            if(!challenge.canTransitionState(oldState, newState))
+                throw Exception("Cannot transition from ${oldState.type} to ${newState.type}")
+
+            val txResult = GenerateTransactionService.execute(caller, TransactionNamespace(
+                from = challenge.cryptoKeyPair.publicKey,
+                to = userAccount.cryptoKeyPair.publicKey,
+                previousTransaction = oldTx.idValue,
+                metadatas = null,
+                action = ActionNamespace(
+                    type = newState,
+                    data = challenge.idValue,
+                    dataType = Challenge::class.simpleName!!
+                )
+            ), null)
+
+            if(txResult.result != SOAResultType.SUCCESS)
+                throw Exception(txResult.message)
+            return@execute txResult.data!!
+        }
     }
 }
