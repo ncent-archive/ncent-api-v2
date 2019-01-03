c05a5a9ed60be7a83f1c06c634ecdd3f5379b972
Adam Foosaner <nucat176@gmail.com>
2019-01-02 16:39:04 -0800

    Adds GetUserAccountService and corresponding unit test

---
+++ src/main/services/user_account/GetUserAccountService.kt
@@ -0,0 +1,30 @@
+package main.services.user_account
+
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAResultType
+import kotlinserverless.framework.services.SOAServiceInterface
+import main.daos.*
+import framework.services.DaoService
+import org.jetbrains.exposed.sql.select
+
+object GetUserAccountService: SOAServiceInterface<UserAccount> {
+    override fun execute(caller: Int?, id: Int?, params: Map<String, String>?): SOAResult<UserAccount> {
+        val result = SOAResult<UserAccount>(
+                SOAResultType.FAILURE,
+                "",
+                null
+        )
+
+        val userAccountResult = DaoService.execute {
+            val query = UserAccounts
+                .select {
+                    UserAccounts.id eq id!!
+                }.withDistinct()
+            UserAccount.wrapRows(query).toList().distinct()
+        }
+
+        if (userAccountResult.result != SOAResultType.SUCCESS)
+            return SOAResult(userAccountResult.result, userAccountResult.message, null)
+        return SOAResult(SOAResultType.SUCCESS, null, userAccountResult.data!!.first())
+    }
+}
