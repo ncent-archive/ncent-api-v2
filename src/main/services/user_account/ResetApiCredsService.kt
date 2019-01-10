4824316e70915db83e4654bdd2523a9563a3574e
mikebarile <mikebarile13@gmail.com>
2019-01-10 11:49:52 -0800

    Wrote ResetApiCredsService.

--- src/main/services/user_account/ResetApiCredsService.kt
+++ src/main/services/user_account/ResetApiCredsService.kt
@@ -1,2 +1,34 @@
 package main.services.user_account
 
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAResultType
+import kotlinserverless.framework.services.SOAServiceInterface
+import main.daos.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.lang.Exception
+
+/**
+ * This service will be used to reset a UserAccount's ApiCreds
+ */
+object ResetApiCredsService: SOAServiceInterface<ApiCred> {
+    override fun execute(caller: Int?) : SOAResult<ApiCred> {
+        val apiCredResult = GenerateApiCredsService.execute()
+        if(apiCredResult.result != SOAResultType.SUCCESS)
+            return SOAResult(apiCredResult.result, apiCredResult.message, null)
+        val apiCredNamespace: ApiCredNamespace = apiCredResult.data!!
+        val apiCreds = ApiCred.new {
+            apiKey = apiCredNamespace.apiKey
+            secretKey = apiCredNamespace.secretKey
+        }
+
+        return try {
+            val userAccount = UserAccount.findById(caller!!)!!
+            transaction {
+                userAccount.apiCreds= apiCreds
+            }
+            SOAResult(SOAResultType.SUCCESS, null, userAccount.apiCreds)
+        } catch(e: Exception) {
+            SOAResult(SOAResultType.FAILURE, e.message, null)
+        }
+    }
+}
