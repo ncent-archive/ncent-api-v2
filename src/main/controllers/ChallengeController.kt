0dc1d57730120553ae99042de902a402212ab3fd
Adam Foosaner <nucat176@gmail.com>
2019-01-10 19:08:11 -0800

    begins challenges controller implementation

--- src/main/controllers/ChallengeController.kt
+++ src/main/controllers/ChallengeController.kt
@@ -3,13 +3,47 @@ package main.controllers
 import kotlinserverless.framework.controllers.RestController
 import kotlinserverless.framework.controllers.DefaultController
 import kotlinserverless.framework.services.SOAResult
-import main.daos.Challenge
 import kotlinserverless.framework.models.Request
-import main.daos.UserAccount
+import main.daos.*
+import main.services.user_account.ValidateApiKeyService
+import main.services.challenge.*
+import main.helpers.JsonHelper
+import org.joda.time.DateTime
 
 class ChallengeController: DefaultController<Challenge>(), RestController<Challenge, UserAccount> {
-    override fun create(user: UserAccount, params: Map<String, String>): SOAResult<*> {
-        throw NotImplementedError()
+    override fun create(user: UserAccount, params: Map<String, String>): SOAResult<Challenge> {
+        val apiCred = user.apiCreds
+
+        ValidateApiKeyService.execute(apiCred.apiKey, apiCred.secretKey)
+
+        val parentChallenge: Int? = params["parentChallenge"]?.toInt()
+
+        val challengeSettings = JsonHelper.parse(params["challengeSettings"]!!)
+        val challengeSettingNamespace = ChallengeSettingNamespace(
+                challengeSettings["name"] as String,
+                challengeSettings["description"] as String,
+                challengeSettings["imageUrl"] as String,
+                challengeSettings["sponsorName"] as String,
+                challengeSettings["expiration"] as DateTime,
+                challengeSettings["shareExpiration"] as DateTime,
+                challengeSettings["admin"] as Int,
+                challengeSettings["maxShares"] as Int,
+                challengeSettings["offChain"] as Boolean,
+                challengeSettings["maxRewards"] as Int?,
+                challengeSettings["maxDistributionFeeReward"] as Int?,
+                challengeSettings["maxSharesPerReceivedShare"] as Int?,
+                challengeSettings["maxDepth"] as Int?,
+                challengeSettings["maxNodes"] as Int?
+        )
+        GenerateChallengeSettingsService.execute(user, challengeSettingNamespace)
+
+        val completionCriteria = JsonHelper.parse(params["completionCriteria"]!!)
+        //Todo: parse and form prerequisites list for completion criteria namespace
+        val completionCriteriaNamespace = CompletionCriteriaNamespace(
+
+        )
+
+
     }
 
     override fun findOne(user: UserAccount, id: Int): SOAResult<Challenge> {
