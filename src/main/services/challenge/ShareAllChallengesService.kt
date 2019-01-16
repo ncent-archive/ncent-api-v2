9903447d76b18df49e8b372b0b0bd4ff8f1ba8a4
mikebarile <mikebarile13@gmail.com>
2019-01-16 17:13:11 -0600

    Add ShareAllChallengesService and tests.

---
+++ src/main/services/challenge/ShareAllChallengesService.kt
@@ -0,0 +1,48 @@
+package main.services.challenge
+
+import kotlinserverless.framework.services.SOAResult
+import kotlinserverless.framework.services.SOAResultType
+import main.daos.*
+import main.helpers.UserAccountHelper
+
+/**
+ * Share a challenge.
+ */
+object ShareAllChallengesService {
+    fun execute(
+            caller: UserAccount,
+            publicKeyToShareWith: String? = null,
+            emailToShareWith: String? = null
+    ) : SOAResult<Pair<TransactionList, NewUserAccount?>> {
+
+        // Validate user exists or attempt to generate a new user.
+        val publicKeyAndAccount = UserAccountHelper.getOrGenerateUser(emailToShareWith, publicKeyToShareWith)
+        val(publicKeyToShareWith, newUserAccount) =
+                if(publicKeyAndAccount.result != SOAResultType.SUCCESS)
+                    return SOAResult(publicKeyAndAccount.result, publicKeyAndAccount.message)
+                else
+                    publicKeyAndAccount.data!!
+
+        // Pull list of non-transferred shares.
+        val unsharedTransactions = GetUnsharedTransactionsService.execute(caller)
+        if(unsharedTransactions.result != SOAResultType.SUCCESS)
+            return SOAResult(SOAResultType.FAILURE, unsharedTransactions.message)
+
+        // Iterate through non-transferred shares and send balances to new public key address.
+        var shareTransactions = mutableListOf<Transaction>()
+        unsharedTransactions.data!!.transactionsToShares.forEach {
+            val challenge = Challenge.findById(it.first.action.data)!!
+            var result = ShareChallengeService.execute(
+                    caller, challenge, it.second, publicKeyToShareWith, emailToShareWith)
+            if(result.result != SOAResultType.SUCCESS)
+                return SOAResult(SOAResultType.FAILURE, result.message, result.data)
+
+            shareTransactions.addAll(result.data!!.first.transactions)
+        }
+
+        return SOAResult(
+                SOAResultType.SUCCESS,
+                "Successfully transferred all challenge shares.",
+                Pair(TransactionList(shareTransactions), publicKeyAndAccount.data!!.second))
+    }
+}
