70b0b1968227e4f8c012c55086695001db6ec51b
Arya Soltanieh <soltanieharya+github@gmail.com>
2019-01-23 13:21:41 -0800

    Fix for local

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ mvn clean compile assembly:single
 serverless deploy --stage $1
 
 # Not required, but good way to test if it is up and running properly
-serverless invoke $1 --function user
+serverless invoke $1 --function user --data '{"path": "/user_test/hello"}'
