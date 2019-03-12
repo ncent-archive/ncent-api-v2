68afe57673d9f20598a72e7cf68439ef72502214
Arya Soltanieh <soltanieharya+github@gmail.com>
2019-03-12 19:48:27 -0400

    remove daoservice execute from all controllers

--- src/main/controllers/ChallengeController.kt
+++ src/main/controllers/ChallengeController.kt
@@ -1,10 +1,8 @@
 package main.controllers
 
 import com.beust.klaxon.JsonObject
-import framework.services.DaoService
 import kotlinserverless.framework.controllers.RestController
 import kotlinserverless.framework.controllers.DefaultController
-import kotlinserverless.framework.models.ForbiddenException
 import kotlinserverless.framework.services.SOAResult
 import kotlinserverless.framework.services.SOAResultType
 import main.daos.*
@@ -15,13 +13,9 @@ import main.helpers.ControllerHelper.RequestData
 
 class ChallengeController: DefaultController<Challenge>(), RestController<Challenge, UserAccount> {
     override fun create(user: UserAccount?, requestData: RequestData): SOAResult<Challenge> {
-        val generateChallengeResult = DaoService.execute {
-            val challengeNamespace = JsonHelper.parse<ChallengeNamespace>(requestData.body["challengeNamespace"]!! as JsonObject)
+        val challengeNamespace = JsonHelper.parse<ChallengeNamespace>(requestData.body["challengeNamespace"]!! as JsonObject)
 
-            return@execute GenerateChallengeService.execute(user!!, challengeNamespace)
-        }
-        DaoService.throwOrReturn(generateChallengeResult)
-        return generateChallengeResult.data!!
+        return GenerateChallengeService.execute(user!!, challengeNamespace)
     }
 
     override fun findOne(user: UserAccount, requestData: RequestData, id: Int?): SOAResult<Challenge> {
@@ -45,28 +39,17 @@ class ChallengeController: DefaultController<Challenge>(), RestController<Challe
         val challengeId = requestData.body["challengeId"] as Int
         val challenge = ChallengeHelper.findChallengeById(challengeId)
 
-        val completeResult = DaoService.execute {
-            return@execute CompleteChallengeService.execute(user!!, challenge, completerPublicKey)
-        }
-
-        DaoService.throwOrReturn(completeResult)
-        return completeResult.data!!
+        return CompleteChallengeService.execute(user!!, challenge, completerPublicKey)
     }
 
-    @Throws(ForbiddenException::class)
     fun redeem(user: UserAccount?, requestData: RequestData): SOAResult<TransactionList> {
         val completerPublicKey = requestData.body["completerPublicKey"] as String
         val challengeId = requestData.body["challengeId"] as Int
         val challenge = ChallengeHelper.findChallengeById(challengeId)
 
-        val redeemResult = DaoService.execute {
-            RedeemChallengeService.execute(user!!, challenge, completerPublicKey)
-        }
-        DaoService.throwOrReturn(redeemResult)
-        return redeemResult.data!!
+        return RedeemChallengeService.execute(user!!, challenge, completerPublicKey)
     }
 
-    @Throws(ForbiddenException::class)
     fun share(user: UserAccount?, requestData: RequestData): SOAResult<TransactionWithNewUser> {
         val challengeId = requestData.body["challengeId"] as String
         val publicKeyToShareWith = requestData.body["publicKeyToShareWith"] as String?
@@ -76,22 +59,12 @@ class ChallengeController: DefaultController<Challenge>(), RestController<Challe
 
         val challenge = ChallengeHelper.findChallengeById(challengeId.toInt())
 
-        val shareResult = DaoService.execute {
-            ShareChallengeService.execute(user!!, challenge, shares, publicKeyToShareWith, emailToShareWith, expiration)
-        }
-
-        DaoService.throwOrReturn(shareResult)
-        return shareResult.data!!
+        return ShareChallengeService.execute(user!!, challenge, shares, publicKeyToShareWith, emailToShareWith, expiration)
     }
 
     fun balances(user: UserAccount?, requestData: RequestData): SOAResult<EmailToChallengeBalanceList> {
         val challengeId = requestData.queryParams["challengeId"] as String
 
-        val balancesResult = DaoService.execute {
-            return@execute GetAllBalancesForChallengeService.execute(user!!, challengeId.toInt())
-        }
-
-        DaoService.throwOrReturn(balancesResult)
-        return balancesResult.data!!
+        return GetAllBalancesForChallengeService.execute(user!!, challengeId.toInt())
     }
 }
